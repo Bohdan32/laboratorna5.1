66159fa668ba17d03201eaba10a9a8bfbc24caf6
Bohdan32 <48551561+Bohdan32@users.noreply.github.com>
2021-10-20 16:31:43 +0300

    Add files via upload

---
+++ UnitTest2.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double p, q;
+			cout << "p = "; cin >> p;
+			cout << "q = "; cin >> q;
+			double t;
+			t = (k(1 + p, q * q) * k(1 + p, q * q) - k(q * p, 1)) / (1 + k(p * p, q));
+			Assert::AreEqual(t, -0.122202);
+
+		}
+	};
+}
